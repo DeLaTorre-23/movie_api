6dd37c576967bf7c5399678af4c158f6a1b0261e
Serch <ztorre23@gmail.com>
2020-11-03 14:37:02 +0100

    [add] top movies to API

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+  app = express(),
+  morgan = require('morgan')
+  ;
 
-let topBooks = [
+let topMovies = [
   {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
+    title: 'Enron, the smartest guys in the room',
+    author: 'Alex Gibney'
   },
   {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
+    title: 'Bowling for Columbine',
+    author: 'Michael Moore'
   },
   {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
+    title: 'Food, Inc',
+    author: 'Robert Kenner'
+  },
+  {
+    title: 'Inside Job',
+    author: 'Charles Ferguson'
+  },
+  {
+    title: 'Citizenfour',
+    author: 'Laura Poitras'
+  },
+  {
+    title: 'Cowspiracy',
+    author: 'Kip Andersen'
+  },
+  {
+    title: 'Ícaro',
+    author: 'Bryan Fogel'
+  },
+  {
+    title: 'Our Planet',
+    author: 'Alastair Fothergill'
+  },
+  {
+    title: 'Terra',
+    author: 'Michael Pitiot'
+  },
+  {
+    title: 'What the health',
+    author: 'Kip Andersen'
   }
 ];
 
@@ -22,26 +52,95 @@ let myLogger = (req, res, next) => {
 };
 
 app.use(myLogger);
+app.use(express.static('public'));
+app.use(morgan('common'));
 
 // GET requests
 app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
-});
-
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret URL with super top-secret contetn.');
+  res.send('Enjoy the thousands of movies on MyFlix!');
 });
 
 app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get('/books', (req, res) => {
-  res.json(topBooks);
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
 });
-
 
 // listen for requests
 app.listen(8080, () =>
 console.log('Your app is listening on port 8080.')
 );
+
+
+
+
+
+
+
+
+// no touch
+
+//
+//
+// const http = require('http'),
+// url = require('url');
+//
+// http.createServer((request, resposnse) => {
+//   let requestURL = url.parse(request.url, true);
+//   if (request.URL.pathname = '/documentation.html') {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Documentation on the bookclub API.\n');
+//   } else {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Welcome to my book club!\n');
+//   }
+// }).listen(8080);
+//
+// console.log('My first Node test server is running on Port 8080.');
+//
+//
+
+
+// no touch
+//
+//
+//
+// const express = require('express'),
+//   morgan = require('morgan'),
+//   app = express();
+//
+// let topBooks = [
+//   {
+//     title: 'Harry Potter and the Sorcerer\'s Stone',
+//     author: 'J.K. Rowling'
+//   },
+//   {
+//     title: 'Lord of the Rings',
+//     author: 'J.R.R. Tolkien'
+//   },
+//   {
+//     title: 'Twilight',
+//     author: 'Stephanie Meyer'
+//   }
+// ];
+//
+//
+// app.use(morgan('common'));
+//
+// app.get('/', (req, res) => {
+//   res.send('Welcome to my app!');
+// });
+//
+// app.get('/secreturl', (req, res) => {
+//   res.send('This is a secret url with super top-secret content.');
+// });
+//
+// app.get('/books', (req, res) => {
+//   res.json(topBooks);
+// });
+//
+// app.listen(8080, () => {
+//   console.log('Your app is listening on port 8080.');
+// });
